b401a50be8deb7e438a5cd96c81cf3825d1fb725
Jose Perez-Roura <jose.perezroura@gmail.com>
2017-06-07 14:08:11 -0400

    new view file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var express = require('express');
+var app = express();
+var path = require('path');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// viewed at http://localhost:3000
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000);
